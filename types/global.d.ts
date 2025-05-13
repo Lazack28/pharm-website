5eba7a579743852b2c38ac7c3b27b784a79a4a9c
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-05-14 00:24:42 +0300

    MTAJU

--- types/global.d.ts
+++
@@ -1,8 +0,0 @@
-interface Window {
-    emailjs: {
-      init: (publicKey: string) => void
-      sendForm: (serviceId: string, templateId: string, form: HTMLFormElement) => Promise<any>
-      send: (serviceId: string, templateId: string, templateParams: any) => Promise<any>
-    }
-  }
-  
